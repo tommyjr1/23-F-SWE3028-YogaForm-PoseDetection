883857761ca0ae997640344e096d6e9b4cda1bd1
Jaehyun Song <tommyjr1@naver.com>
2023-10-20 03:13:04 +0900

    init and spring be

---
+++ backend/yf_be/src/main/java/com/example/yf_be/YfBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yf_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YfBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YfBeApplication.class, args);
+	}
+
+}
---
+++ backend/yf_be/src/test/java/com/example/yf_be/YfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yf_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YfBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
