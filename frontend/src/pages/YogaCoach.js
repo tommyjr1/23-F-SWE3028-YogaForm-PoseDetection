bc452b29e3758324b520237ee1c21bd3e1c66b67
jaehyun <tommyjr1@naver.com>
2023-11-09 21:38:28 +0000

    merging

--- frontend/src/pages/YogaCoach.js
+++ frontend/src/pages/YogaCoach.js
@@ -31,84 +31,79 @@ const YogaCoach = () => {
     let userPoseAngle = null;
     const [message, setMessage] = useState("");
 
-    const canvasElement = canvasRef.current;
-    const canvasCtx = canvasElement.getContext("2d");
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
-    drawConnectors(
-      canvasCtx,
-      results.poseLandmarks,
-      mediapipePose.POSE_CONNECTIONS,
-      { color: "white", lineWidth: 1 }
-    );
-    // * The dots are the landmarks
-    drawLandmarks(canvasCtx, results.poseLandmarks, {
-      color: "red",
-      lineWidth: 1,
-      radius: 2,
-    });
-    canvasCtx.restore();
-
-    const calculatePoseAngle = (a, b, c) => {
-      let radians = Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x) // * fetching the radians using the atan2 function 
-      let angle = radians * (180 / Math.PI) // * calculating the angle from the radian
-      // need to provide dynamic values for angles as per requirement later along with the number of reps.
-      if (angle > 180) { // * if the angle is greater than 180, then it is negative so changing it back to positive or an actual angle possible for a human being, lol..
-          angle = 360 - angle
-      }
-      if (angle > 0 && angle < 180) { // * if the angle is positive, then it is a positive angle
-          // console.log(angle.toFixed(2), "currentAngle");
-      }
-      userPoseAngle = angle.toFixed(2);
-
-      
-      // console.log(userPoseAngle);
-      if(userPoseAngle!=null){
-        submitAngleData();
-
-      }
-      // calculateReps(userPoseAngle);
-      getCheckError();
-    }
-
-    // getApi = () => {
-    //   axios.get("http://3.35.60.125:8080")
-    //       .then(res => {
-    //           console.log(res);
-    //           this.setState({
-    //               message: res.data.message
-    //           })
-    //       })
-    //       .catch(res => console.log(res))
-    // }
+    function onResults(results) {
+      let landmarks = results.poseLandmarks // * all the landmarks in the pose
   
-    const getCheckError = async () => {
+      //  * getting the values for the three landmarks that we want to use
+      try { // * we get errors every time the landmarks are not available
+          // * will provide dynamic landmarks later "landmarks[mediapipePose.POSE_LANDMARKS.{landmark}]"
+          let leftShoulder = landmarks[mediapipePose.POSE_LANDMARKS.LEFT_SHOULDER];
+          let leftElbow = landmarks[mediapipePose.POSE_LANDMARKS.LEFT_ELBOW];
+          let leftWrist = landmarks[mediapipePose.POSE_LANDMARKS.LEFT_WRIST];
+          calculatePoseAngle(leftShoulder, leftElbow, leftWrist);
+      } catch (error) {
+          // console.error(error);
+      }
+      // Define the canvas element dimensions using the earlier created refs
+      canvasRef.current.width = webcamRef.current.video.videoWidth
+      canvasRef.current.height = webcamRef.current.video.videoHeight
+  
+      const canvasElement = canvasRef.current;
+      const canvasCtx = canvasElement.getContext("2d")
+      canvasCtx.save();
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      canvasCtx.drawImage(results.image,
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+      )
+      drawConnectors(canvasCtx,
+          results.poseLandmarks, mediapipePose.POSE_CONNECTIONS,
+          { color: 'white', lineWidth: 1 });
+      // * The dots are the landmarks 
+      drawLandmarks(canvasCtx, results.poseLandmarks,
+          { color: 'red', lineWidth: 1, radius: 2 });
+      canvasCtx.restore();
+  }
 
-      await axios
-      .get("http://3.35.60.125:8080/api/check")
-      .then((response)=>{
-        console.log(response.data)
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
 
+  const calculatePoseAngle = (a, b, c) => {
+    let radians =
+      Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x); // * fetching the radians using the atan2 function
+    let angle = radians * (180 / Math.PI); // * calculating the angle from the radian
+    // need to provide dynamic values for angles as per requirement later along with the number of reps.
+    if (angle > 180) {
+      // * if the angle is greater than 180, then it is negative so changing it back to positive or an actual angle possible for a human being, lol..
+      angle = 360 - angle;
     }
+    if (angle > 0 && angle < 180) {
+      // * if the angle is positive, then it is a positive angle
+      // console.log(angle.toFixed(2), "currentAngle");
+    }
+    userPoseAngle = angle.toFixed(2);
+    // console.log(userPoseAngle);
+    submitAngleData();
+    // calculateReps(userPoseAngle);
+  };
+
+  // getApi = () => {
+  //   axios.get("http://3.35.60.125:8080")
+  //       .then(res => {
+  //           console.log(res);
+  //           this.setState({
+  //               message: res.data.message
+  //           })
+  //       })
+  //       .catch(res => console.log(res))
+  // }
+
     const submitAngleData = async () => {
       console.log(typeof userPoseAngle);
 
       await axios
       .post("http://3.35.60.125:8080/api/angle",{
-          value: userPoseAngle,
-    // private String part;
-    // private String check;
+          angle: userPoseAngle
       })
       .then((response)=>{
         console.log(response.data)
