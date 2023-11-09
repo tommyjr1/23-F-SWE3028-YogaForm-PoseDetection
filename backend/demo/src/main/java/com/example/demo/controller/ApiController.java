a6acd34c08d9ee83064ac4a7f0fdf7cd5b9b54d6
jaehyun <tommyjr1@naver.com>
2023-11-09 17:38:30 +0000

    post

--- backend/demo/src/main/java/com/example/demo/controller/ApiController.java
+++ backend/demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +14,7 @@ public class ApiController {
         System.out.println(angle);
     }
     @PostMapping("/api/angle")
-    public void postData(@RequestParam String angle){
+    public void postData(@RequestBody String angle){
         System.out.println(angle);
     }
     // public HashMap hello() {
