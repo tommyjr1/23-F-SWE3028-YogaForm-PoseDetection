edf9d6d8b2544cd43e27487c7f5e7edbd90e33c6
hyeeiin <hyeinsally@g.skku.edu>
2023-11-24 01:27:03 +0900

    put webcamref in conditional header

--- frontend/src/components/ConditionalHeader.js
+++ frontend/src/components/ConditionalHeader.js
@@ -5,36 +5,48 @@ import LogInPage from '../pages/LogInPage';
 import MyPage from '../pages/MyPage';
 import queryString from 'query-string'
 
-export default function ConditionalHeader(props){
+export default function ConditionalHeader(props) {
 
     const buttonStyle = {
-        backgroundColor:"#F2CCFF", 
+        backgroundColor: "#F2CCFF",
         border: "1px solid #F2CCFF",
         color: "#3B2C77",
         fontSize: "1.6rem"
     }
     const navigate = useNavigate();
     const goToLogInPage = () => {
-    navigate("/LogInPage");
+        stopWebCam();
+        navigate("/LogInPage");
     }
     const goToMyPage = () => {
-    navigate("/MyPage");
+        stopWebCam();
+        navigate("/MyPage");
     }
 
+    const stopWebCam = () => {
+        const webcamRef = props.webcamRef;
+        if (webcamRef.current.video) {
+            let stream = webcamRef.current.video.srcObject;
+            const tracks = stream.getTracks();
+            tracks.forEach((track) => track.stop());
+            webcamRef.current.video.srcObject = null;
+        }
+    };
+
     function UserHeader() {
-        return <header style={{display: "flex", flexDirection: "row", paddingTop: "1rem",paddingBottom: "1rem", justifyContent:"space-around"}}><div style={{fontWeight: "bold", fontSize: "1.8rem"}}>YOGA FORM</div><div></div><div></div><div></div><div></div><div></div>
-        <Button variany="secondary" style={buttonStyle}>HOME</Button><Button variany="secondary" style={buttonStyle}>ABOUT</Button><Button variany="secondary" style={buttonStyle}>YOGA</Button><Button variany="secondary" style={{backgroundColor: "#FFF2CC", border: "1px solid #FFF2CC",borderRadius: '2rem', width: "120px", color: "#3B2C77",fontSize: "1.6rem"}} onClick={goToMyPage}>My page</Button></header>;
-      }
-      
+        return <header style={{ display: "flex", flexDirection: "row", paddingTop: "1rem", paddingBottom: "1rem", justifyContent: "space-around" }}><div style={{ fontWeight: "bold", fontSize: "1.8rem" }}>YOGA FORM</div><div></div><div></div><div></div><div></div><div></div>
+            <Button variany="secondary" style={buttonStyle}>HOME</Button><Button variany="secondary" style={buttonStyle}>ABOUT</Button><Button variany="secondary" style={buttonStyle}>YOGA</Button><Button variany="secondary" style={{ backgroundColor: "#FFF2CC", border: "1px solid #FFF2CC", borderRadius: '2rem', width: "120px", color: "#3B2C77", fontSize: "1.6rem" }} onClick={goToMyPage}>My page</Button></header>;
+    }
+
     function GuestHeader() {
-    return <header style={{display: "flex", flexDirection: "row", paddingTop: "1rem",paddingBottom: "1rem", justifyContent:"space-around"}}><div style={{fontWeight: "bold", fontSize: "1.8rem"}}>YOGA FORM</div><div></div><div></div><div></div><div></div><div></div>
-    <Button variany="secondary" style={buttonStyle}>HOME</Button><Button variany="secondary" style={buttonStyle}>ABOUT</Button><Button variany="secondary" style={buttonStyle}>YOGA</Button><Button variany="secondary" style={{backgroundColor: "#FFF2CC", border: "1px solid #FFF2CC",borderRadius: '2rem', width: "100px", color: "#3B2C77",fontSize: "1.6rem"}} onClick={goToLogInPage}>Log-in</Button></header>;
+        return <header style={{ display: "flex", flexDirection: "row", paddingTop: "1rem", paddingBottom: "1rem", justifyContent: "space-around" }}><div style={{ fontWeight: "bold", fontSize: "1.8rem" }}>YOGA FORM</div><div></div><div></div><div></div><div></div><div></div>
+            <Button variany="secondary" style={buttonStyle}>HOME</Button><Button variany="secondary" style={buttonStyle}>ABOUT</Button><Button variany="secondary" style={buttonStyle}>YOGA</Button><Button variany="secondary" style={{ backgroundColor: "#FFF2CC", border: "1px solid #FFF2CC", borderRadius: '2rem', width: "100px", color: "#3B2C77", fontSize: "1.6rem" }} onClick={goToLogInPage}>Log-in</Button></header>;
     }
 
     const isLoggedIn = props.isLoggedIn;
     if (isLoggedIn) {
         return <UserHeader />;
-    }else{
+    } else {
         return <GuestHeader />;
     }
 }
