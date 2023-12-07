9537d0028f23d157dd9149968f2afb71ad0f490c
yuuhaaaaa <yuuhaaaaa19@gmail.com>
2023-12-08 03:50:19 +0900

    modify yogalist

--- frontend/src/pages/YogaList.js
+++ frontend/src/pages/YogaList.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import queryString from "query-string";
-import React, { useEffect, useState, Children } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import ConditionalHeader from "../components/ConditionalHeader";
 import { create } from "lodash";
-import { setLabels } from "react-chartjs-2/dist/utils";
 
 const YogaList = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,6 +13,7 @@ const YogaList = () => {
     top: 0,
     left: 0,
     width: "100%",
+    height: "100%",
     backgroundColor: "#F2CCFF",
     color: "#3B2C77",
   };
@@ -23,28 +23,51 @@ const YogaList = () => {
     color: "#3B2C77",
     fontSize: "1.6rem",
   };
+
   const [x, setX] = useState(false);
   const [imgUrls, setImgurl] = useState([]);
-  // let imgUrls2 = [];
   const [poseName, setPoseName] = useState([]);
+  const [checkedList, setCheckedList] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkedItemHandler = (value, isChecked) => {
+    if (isChecked) {
+      setCheckedList((prev) => [...prev, value]);
+      return;
+    }
+    if (!isChecked && checkedList.includes(value)) {
+      setCheckedList(checkedList.filter((item) => item !== value));
+      return;
+    }
+    return;
+  };
+
+  const checkHandler = (e, value) => {
+    setIsChecked(!isChecked);
+    checkedItemHandler(value, e.target.checked);
+
+    console.log(value, e.target.checked);
+  };
 
   const getPoseName = async () => {
     await axios
       .get("http://3.35.60.125:8080/yf/pose/getName")
       .then((res) => {
+        //poseNames = res.data;
+        // res.data.forEach(item => {
+        //   poseNames.push(item);
+        // });
         setPoseName(res.data);
-        //setPoseName(prevList => [prevList, res.data]);
-        // const data = res.data;
-        // setPoseName(data);
-        // return data;
       })
       .catch((err) => {
         console.log(err);
-        // return [];
       });
-    // poseName.forEach((item) => {
-    //   getImage(item);
-    // });
+  };
+
+  const onSubmit = (e) => {
+    // console.log("checkedList: ", checkedList);
+    e.preventDefault();
+    console.log("checkedList: ", checkedList);
   };
 
   const getImage = async (name) => {
@@ -58,13 +81,15 @@ const YogaList = () => {
           type: "image/png",
         });
         const imgUrl = URL.createObjectURL(blob);
-        // let copy = Array.from(imgUrls);
-        // copy.push(imgUrl);
-        // console.log(imgUrl);
+        //imgUrls.push(imgUrl);
+        // setImgurl(imgUrls.concat([imgUrl]));
+        let copy = imgUrls;
+        console.log(copy);
+        //console.log(copy.length);
+        //let input = copy.push(imgUrl);
+        //console.log(input);
         // setImgurl(copy);
-        // console.log(copy.length);
-        // imgUrls2.push(imgUrl);
-        setImgurl(prevList => [prevList, imgUrl]);
+        setImgurl((prevList) => [prevList, imgUrl]);
         console.log(imgUrls);
       })
       .catch((error) => {
@@ -80,21 +105,23 @@ const YogaList = () => {
         const { isLogin } = queryObj;
         setIsLoggedIn(isLogin === "true");
         const poseNames = await getPoseName();
-        console.log(poseName); //이거왜빈배열???
-        const imagePromises = Array.from(poseNames).map((item) => getImage(item));
+        console.log(poseName); //왜빈배열???
+        const imagePromises = Array.from(poseNames).map((item) =>
+          getImage(item)
+        );
         await Promise.all(imagePromises);
         //console.log(imgUrls2);
-      } catch(error) {
-        console.log("Error: ", error);
+      } catch (e) {
+        console.log("Error: ", e);
         setIsLoggedIn(false);
       }
-      
     };
+
     fetch();
   }, []);
 
-  const poseList= poseName.map((pose, index) => {
-    // console.log(pose);
+  const poseList = poseName.map((pose, index) => {
+    //console.log(imgUrls);
     return (
       <li
         style={{
@@ -123,6 +150,45 @@ const YogaList = () => {
     );
   });
 
+  const checkPoseList = poseName.map((pose, index) => {
+    //console.log(imgUrls);
+    return (
+      <li
+        style={{
+          textAlign: "match-parent",
+          listStyle: "none",
+          margin: "10px",
+        }}
+        key={pose}
+      >
+        <div>
+          <img
+            id={pose}
+            src={imgUrls[index]}
+            style={{
+              overflowClipMargin: "content-box",
+              overflow: "clip",
+              objectFit: "cover",
+              width: "150px",
+            }}
+          ></img>
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="poseTitle">
+            {/* <h6>{pose}</h6> */}
+            <input
+              type="checkbox"
+              id={pose}
+              checked={checkedList.includes(pose)}
+              onChange={(e) => checkHandler(e, pose)}
+            />
+            <label htmlFor={pose}>{pose}</label>
+          </div>
+        </form>
+      </li>
+    );
+  });
+
   return (
     <div className="App" style={bodyStyle}>
       <ConditionalHeader isLoggedIn={isLoggedIn}></ConditionalHeader>
@@ -135,7 +201,8 @@ const YogaList = () => {
         }}
       >
         <h1 style={{ paddingLeft: "40px" }}>Standing</h1>
-        <button style={{
+        <button
+          style={{
             backgroundColor: "#FFF2CC",
             border: "1px solid #FFF2CC",
             borderRadius: "2rem",
@@ -145,13 +212,32 @@ const YogaList = () => {
             fontSize: "1rem",
             flex: 1,
             flexDirection: "row",
-            alignItems: "flex-end"
+            alignItems: "flex-end",
           }}
-          
-          >Make a routine</button>
+        >
+          Make a routine
+        </button>
         <ul style={{ display: "flex", flexWrap: "wrap" }}>
-          {poseList}
+          {/* {poseList} */}
+          {checkPoseList}
         </ul>
+        <button
+          style={{
+            backgroundColor: "#FFF2CC",
+            border: "1px solid #FFF2CC",
+            borderRadius: "2rem",
+            width: "120px",
+            height: "40px",
+            color: "#3B2C77",
+            fontSize: "1rem",
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "flex-end",
+          }}
+          type="submit"
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
